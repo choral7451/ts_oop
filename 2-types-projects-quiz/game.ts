a64707bd3808ba1266925b1caf21d23a75f3d7e9
choral7451 <chorales@naver.com>
2022-05-15 22:08:32 +0900

    좌표 게임

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,6 +1,16 @@
 /**
  * Let's make a game 🕹
  */
+let position = { x: 0, y: 0};
+type Move = 'up' | 'down' | 'left' | 'right';
+function move (direction: Move) {
+    if(direction === 'up') return position.y++;
+    else if(direction === 'down') return position.y--;
+    else if(direction === 'left') return position.x--;
+    else if(direction === 'right') return position.x++;
+    else throw Error('direction Error')
+}
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
