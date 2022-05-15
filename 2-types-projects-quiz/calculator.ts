119194fa5bdb5863829ecc1b6765c5cf0fb87fe9
choral7451 <chorales@naver.com>
2022-05-15 21:59:17 +0900

    계산기 함수 만들기

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,8 +1,22 @@
 /**
  * Let's make a calculator 🧮
  */
-console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
-console.log(calculate('multiply', 4, 2)); // 8
-console.log(calculate('divide', 4, 2)); // 2
-console.log(calculate('remainder', 5, 2)); // 1
+ console.log(calculate('add', 1, 3)); // 4
+ console.log(calculate('substract', 3, 1)); // 2
+ console.log(calculate('multiply', 4, 2)); // 8
+ console.log(calculate('divide', 4, 2)); // 2
+ console.log(calculate('remainder', 5, 2)); // 1
+ 
+type Command = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+function calculate (str: Command, x: number, y: number): number {
+    if(str === "add") return x + y;
+    else if(str === "substract") return x - y;
+    else if(str === "multiply") return x * y;
+    else if(str === "divide") return x / y;
+    else if(str === "remainder") return x % y;
+    else throw Error('unkown command');
+}
+
+
+
+
