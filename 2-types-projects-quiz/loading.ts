3aee7c96218800ba295918376267fcbcba63e477
choral7451 <chorales@naver.com>
2022-05-15 22:16:53 +0900

    로딩 상태표시

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -20,6 +20,17 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  function printLoginState(ressource: ResourceLoadState) {
+    if(ressource.state === 'loading') console.log('👀 loading...')
+    else if (ressource.state === 'success') {
+      console.log(`content : ${ressource.response.body}`);
+      console.log('😃 loaded')
+    } else if ( ressource.state === 'fail') {
+      console.log(`reason : ${ressource.reason}`);
+      console.log('😱 no network')
+    } else throw Error(`resource Error ${ressource}`)
+  }
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
